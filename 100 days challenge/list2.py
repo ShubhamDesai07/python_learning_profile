613ebbc49628fb45e945631175c3a34c6b14ce02
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-01-29 09:52:50 +0530

    list

---
+++ 100 days challenge/list2.py
@@ -0,0 +1,2 @@
+shopping_list = ['apples','oranges','bananas','cheese']
+print(shopping_list[0:3])
---
+++ 100 days challenge/list3.py
@@ -0,0 +1,13 @@
+shopping_list = ['apples','oranges','bananas','cheese']
+shopping_list.append('blueberries')
+shopping_list[0] = "cherries"
+del shopping_list[1]
+print(len (shopping_list))
+print(shopping_list)
+
+shopping_list2 = ['bread', 'jam', 'butter']
+print(shopping_list2)
+
+print(shopping_list + shopping_list2)
+
+print(shopping_list * 2 )
