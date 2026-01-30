58bc2f646e439e59d01c240e256442e9298832bd
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-01-30 19:46:11 +0530

    d4

---
+++ 100 days challenge/ex05.py
@@ -0,0 +1,4 @@
+num = [10, 20,30,]
+print(len(num))
+print(max(num))
+print(min(num))
---
+++ 100 days challenge/ex06.py
@@ -0,0 +1,4 @@
+stu = {'anish': 45,'vishwa': 47,'SID': 50}
+print(stu['anish'])
+print(stu['SID'])
+print(stu['vishwa'])
