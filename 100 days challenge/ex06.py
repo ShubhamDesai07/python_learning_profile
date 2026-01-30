cb62e8cbe01bc8657ded6e7970e8690080cbf7d5
anishdesai006@gmail.com <anishdesai006@gmail.com>
2026-01-30 19:53:56 +0530

    d4

--- 100 days challenge/ex06.py
+++ 100 days challenge/ex06.py
@@ -1,4 +1,4 @@
-stu = {'anish': 45,'vishwa': 47,'SID': 50}
-print(stu['anish'])
-print(stu['SID'])
-print(stu['vishwa'])
+name_ages = {'anish': 45,'vishwa': 47,'SID': 50}
+
+
+print(name_ages)
---
+++ 100 days challenge/tup.py
@@ -0,0 +1,2 @@
+tup = ('rrr','kantara','taarzan','kantara2' )
+print(tup[0:3])
